26cf933a0272e54066ce940145aaa188bd398182
Debora Silva <deboraro.61@gmail.com>
2023-09-27 19:51:19 -0300

    build: questionario finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -48,23 +48,41 @@ function App() {
     p4:'',  
   })
 
-  function handleChange(event){
-    console.log(event)
+  const [slide, setSlide] = React.useState(0);
+  const [resultado, setResultado] = React.useState(null);
+
+  function handleChange({target}){
+    setRespostas({...respostas, [target.id]:target.value});
+  }
+
+  function resultadoFinal (){
+    const corretas = perguntas.filter(({id, resposta})=> respostas[id] == resposta)
+    setResultado(`Você acertou o total de ${corretas.length} respostas das ${perguntas.length} perguntas`)
+  }
+
+  function handleClick(){
+    if(slide < perguntas.length -1){
+      setSlide(slide + 1);
+    }else{
+      resultadoFinal();
+      setSlide(slide + 1);
+    }
   }
 
   return (
     <>
-      <form id="container">
+      <form id="container" onSubmit={(event)=> event.preventDefault()}>
         <div>
           <a href="https://react.dev" target="_blank">
             <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </div>
-        {perguntas.map((pergunta)=>(
-          <Radio value={respostas[pergunta.id]} onChange={handleChange} key={pergunta.id} {...pergunta}/>
+        {perguntas.map((pergunta, index)=>(
+          <Radio active={slide === index} value={respostas[pergunta.id]} onChange={handleChange} key={pergunta.id} {...pergunta}/>
         ))}
         
-        <button className="btn">Próxima</button>
+        {resultado ? <p>{resultado}</p> : <button onClick={handleClick} className="btn">Próxima</button>}
+        
       </form>
     </>
   )
--- src/Form/Radio.jsx
+++ src/Form/Radio.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
- const Radio = ({pergunta, options, id, onChange, value}) => {
+ const Radio = ({pergunta, options, id, onChange, value, active}) => {
+
+  if (active === false) return null;
+
   return (
     <fieldset style={{fontWeight:'bold', marginBottom:'1rem', border:'2px solid #ade8f4'}}>
       <legend>{pergunta}</legend>
       {options.map((option)=>(
         <label key={option}>
-          <input type='radio' checked={value === option} value={option} onChange={onChange}/>{option}
+          <input id={id} type='radio' checked={value === option} value={option} onChange={onChange}/>{option}
         </label>))}
     </fieldset>
   )
